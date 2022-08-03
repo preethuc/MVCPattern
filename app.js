23d3ac0566e3326e6ea20b7a4a6d26285b64de03
preethuc <preetha@sparkouttech.com>
2022-08-03 23:39:15 +0530

    new files added

--- app.js
+++ app.js
@@ -1,98 +1,22 @@
-const { json } = require('express');
 const express = require('express');
 const app = express();
-const fs = require('fs');
+const morgan = require('morgan');
+const tourRouter = require('./Routes/tourRoutes');
+const userRouter = require('./Routes/userRoutes');
+//1.MIDDLEWARE
+
 app.use(express.json());
-// app.get('/api/',(req,res)=>{
-//    // res.status(200).send('data fetched successfully')
-//    //json data
-//    res.status(200).json({
-//     message:"Datas fetched successfully",
-//     app:"Natours"
-//    })
-// })
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+  console.log('Hello from Middleware');
+  next();
+});
+app.use((req, res, next) => {
+  req.reqestTime = new Date().toISOString();
+  next();
+});
 
-app.get('/api', (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: {
-      tours,
-    },
-  });
-});
-//app.get('/api/tour/:id/:x?',(req,res)=>{   x?--Undefined
-app.get('/api/v1/tour/:id', (req, res) => {
-  console.log(req.params);
-  const id = req.params.id * 1;
-      const tour = tours.find(el => el.id === id);
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
-  if (id > tours.length) {
-   // if(!tour){  these 2 if give the same result
-    return res.status(404).json({
-      status: 'fail',
-      message: 'invalid ID',
-    });
-  }
-  res.status(200).json({
-    status: 'success',
-    //result: tours.length,
-    data:{
-        tour
-    }
-  });
-});
-app.post('/api/v1/tours', (req, res) => {
-  //console.log(req.body);
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
-  //  res.send('Done');
-});
-app.patch('/api/v1/tours/:id',(req,res)=>{
-    if(req.params.id *1 > tours.length){
-        return res.status(404).json({
-            status:'fail',
-            message:'Invalid ID'
-        })
-    }
-    res.status(200).json({
-        status:'success',
-        data:{
-            tour:'<Updated tour here....!>'
-        }
-    })
-})
-app.delete('/api/v1/tours/:id', (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-//   res.status(200).json({
-//     status: 'success',
-//     data: null
-//   });
- res.status(204).json({
-   status: 'success',
-   data: null,
- });
-});
-app.listen(3000, () => console.log('Listening on the PORT 3000'));
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,6 @@
+
+const app = require('./app')
+
+
+//4.START SERVER
+app.listen(3000, () => console.log('Listening on the PORT 3000'));
